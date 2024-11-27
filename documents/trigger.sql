9aa0bb236a743939c966723dd883267c397ae7bc
MiguelAmorim09 <duguerel@gmail.com>
2024-11-26 22:37:05 -0300

    TRIGGER FUNCIONANDO 2.0

--- documents/trigger.sql
+++ documents/trigger.sql
@@ -2,18 +2,17 @@ create trigger emprestimo_tg on SisBib.Emprestimo
 instead of insert
 as
 begin
-	declare @idExemplar int, @devolucaoEfetiva date;
+	declare @idExemplar int;
 
 	select @idExemplar = idExemplar from inserted
 
-	select @devolucaoEfetiva  = devolucaoEfetiva from SisBib.Emprestimo
-	where idExemplar = @idExemplar and devolucaoEfetiva is null
-
-	if(@devolucaoEfetiva is null)
+	if exists(select 1 from SisBib.Emprestimo
+			  where idExemplar = @idExemplar
+			  and devolucaoEfetiva is null)
+		throw 50001, 'Esse exemplar ainda não foi devolvido, não será possível fazer o empréstimo', 1
+	else
 	begin
 		insert into SisBib.Emprestimo(idLeitor, idExemplar, dataEmprestimo, devolucaoPrevista)
 		select idLeitor, idExemplar, dataEmprestimo, devolucaoPrevista from inserted
 	end
-	else
-		throw 50001, 'Esse exemplar ainda não foi devolvido, não será possível fazer o empréstimo', 1
 end
